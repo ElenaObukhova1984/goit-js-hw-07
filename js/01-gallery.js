da512c2a16c1188b2a65e40b735238ec1d7b422d
ElenaObukhova1984 <e.obukhova84@gmail.com>
2022-09-16 21:13:34 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+const marrkupOfGallery = createMarkupOfGallery(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", marrkupOfGallery);
+
+function createMarkupOfGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description}) => `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+    ).join("");
+};
+
+galleryRef.addEventListener("click", handleClick);
+
+function handleClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return
+    }
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+instance.show();
+
+galleryRef.addEventListener("keydown",(event) => {
+    if (event.code === "Escape") {
+        instance.close();    
+}
+}
+)
+
+}
+
+
+    
+
